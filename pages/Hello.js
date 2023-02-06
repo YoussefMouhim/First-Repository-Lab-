f649d0d24b5f2c0fa21362bc82943f67075295d7
YoussefMouhim <youssef.mouhim@hotmail.com>
2023-02-06 20:06:16 +0100

    initial

---
+++ pages/Hello.js
@@ -0,0 +1,11 @@
+const Hello = () => {
+    return ( 
+        <>
+            <h1>Hello World</h1>
+            <p>This is (probably) my first Next App</p>
+        </>
+
+    )
+}
+
+export default Hello;
